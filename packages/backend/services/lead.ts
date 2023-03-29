cfd040b33adeb97fff5f9f2918cd256ee9bd4a1a
jatin <sandilya.jatin@gmail.com>
2023-03-29 15:41:15 +0530

    unify get all leads api

--- packages/backend/services/lead.ts
+++ packages/backend/services/lead.ts
@@ -54,6 +54,60 @@ class LeadService {
             };
         }
     }
+    async getUnifiedLeads(
+        req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>,
+        res: Response<any, Record<string, any>, number>
+    ) {
+        const connection = res.locals.connection;
+        const thirdPartyId = connection.tp_id;
+        const thirdPartyToken = connection.tp_access_token;
+        const tenantId = connection.t_id;
+        const fields = req.query.fields;
+        console.log('Revert::GET ALL LEADS', tenantId, thirdPartyId, thirdPartyToken);
+        if (thirdPartyId === 'hubspot') {
+            let leads: any = await axios({
+                method: 'get',
+                url: `https://api.hubapi.com/crm/v3/objects/contacts?properties=${fields}`,
+                headers: {
+                    authorization: `Bearer ${thirdPartyToken}`,
+                },
+            });
+            leads = filterLeadsFromContactsForHubspot(leads.data.results as any[]);
+            leads = leads?.map((l: any) => unifyLead({ ...l, ...l?.properties }));
+            return {
+                results: leads,
+            };
+        } else if (thirdPartyId === 'zohocrm') {
+            let leads: any = await axios({
+                method: 'get',
+                url: `https://www.zohoapis.com/crm/v3/Leads?fields=${fields}`,
+                headers: {
+                    authorization: `Zoho-oauthtoken ${thirdPartyToken}`,
+                },
+            });
+            leads = leads.data.data;
+            leads = leads?.map((l: any) => unifyLead(l));
+            return { results: leads };
+        } else if (thirdPartyId === 'sfdc') {
+            // NOTE: Handle "ALL" for Hubspot & Zoho
+            const query =
+                fields === 'ALL'
+                    ? 'SELECT+fields(all)+from+Lead+limit+200'
+                    : `SELECT+${(fields as string).split(',').join('+,+')}+from+Lead`;
+            let leads: any = await axios({
+                method: 'get',
+                url: `https://revert2-dev-ed.develop.my.salesforce.com/services/data/v56.0/query/?q=${query}`,
+                headers: {
+                    authorization: `Bearer ${thirdPartyToken}`,
+                },
+            });
+            leads = leads.data?.records;
+            leads = leads?.map((l: any) => unifyLead(l));
+            return { results: leads };
+        } else {
+            return { error: 'Unrecognized CRM' };
+        }
+    }
 }
 
 export default new LeadService();
