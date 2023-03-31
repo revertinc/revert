12a62818d7b2127affda41f189796d98bb062fa3
jatin <sandilya.jatin@gmail.com>
2023-03-31 13:24:07 +0530

    chore: delete crap (ui)

--- packages/client/src/assets/icons/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as HamburgerIcon } from "./HamburgerIcon";
-export { default as PlusCircleIcon } from "./PlusCircleIcon";
-export { default as OverviewIcon } from "./OverviewIcon";
-export { default as FeedbackIcon } from "./FeedbackIcon";
-export { default as ForkIcon } from "./ForkIcon";
