5a965227776a99a37b7dc7784f6f20ac08004cc8
jatin <sandilya.jatin@gmail.com>
2023-03-31 11:40:44 +0530

    chore: delete crap

--- packages/backend/routes/index.ts
+++ packages/backend/routes/index.ts
@@ -1,33 +1,20 @@
-import express from "express";
-import workflowRouter from "./workflow";
-import cronRouter from "./cron";
-import authRouter from "./auth";
-import integrationsRouter from "./integrations";
-import crmRouter from "./crm";
-import nodeRouter from "./node";
-import metadataRouter from "./metadata";
+import express from 'express';
+import crmRouter from './crm';
+import metadataRouter from './metadata';
 
 const router = express.Router();
 
-router.get("/health-check", (_, response) => {
-  response.send({
-    status: "ok",
-  });
+router.get('/health-check', (_, response) => {
+    response.send({
+        status: 'ok',
+    });
 });
 
-router.get("/", (_, response) => {
-  response.send({
-    status: "nothing to see here.",
-  });
+router.get('/', (_, response) => {
+    response.send({
+        status: 'nothing to see here.',
+    });
 });
 
 export default router;
-export {
-  workflowRouter,
-  cronRouter,
-  authRouter,
-  integrationsRouter,
-  nodeRouter,
-  crmRouter,
-  metadataRouter,
-};
+export { crmRouter, metadataRouter };
