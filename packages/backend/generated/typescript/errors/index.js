c604b1aa8ca73ed617cc624973c5ce3aa53e1ee8
jatin <sandilya.jatin@gmail.com>
2023-04-19 16:22:45 +0530

    feat: move metadata api to fern

---
+++ packages/backend/generated/typescript/errors/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RevertApiError = void 0;
+var RevertApiError_1 = require("./RevertApiError");
+Object.defineProperty(exports, "RevertApiError", { enumerable: true, get: function () { return RevertApiError_1.RevertApiError; } });
---
+++ packages/backend/generated/typescript/index.d.ts
@@ -0,0 +1,3 @@
+export * as RevertApi from "./api";
+export { register } from "./register";
+export { RevertApiError } from "./errors";
