f904401e16866c9f5b0184aa2f719ee95fb32835
jatin <sandilya.jatin@gmail.com>
2023-05-13 10:34:05 +0530

    Add hubspot connect demo

--- examples/cra/src/App.js
+++ examples/cra/src/App.js
@@ -1,4 +1,4 @@
-import { RevertConnect } from '@revertdotdev/revert-react';
+import { RevertConnect, useRevertConnect } from '@revertdotdev/revert-react';
 import './App.css';
 
 function App() {
@@ -6,6 +6,12 @@ function App() {
     const REVERT_TOKEN = 'pk_test_Y2xlcmsuc3Ryb25nLmRlZXItNTYubGNsLmRldiQ';
     //This can be any unique string that identifies the user
     const UNIQUE_TENANT_ID = 'testTenantId';
+    const { loading, error, open } = useRevertConnect({
+        config: {
+            revertToken: REVERT_TOKEN,
+            tenantId: UNIQUE_TENANT_ID,
+        },
+    });
     return (
         <div className="App">
             <header className="App-header">
@@ -19,6 +25,23 @@ function App() {
                         tenantId: UNIQUE_TENANT_ID,
                     }}
                 />
+                <button
+                    disabled={loading || Boolean(error)}
+                    id="revert-connect-button2"
+                    onClick={() => open('hubspot')}
+                    style={{
+                        margin: 10,
+                        padding: 10,
+                        outline: 'none',
+                        background: 'rgb(22, 22, 22)',
+                        borderColor: 'rgb(22, 22, 22)',
+                        borderRadius: 5,
+                        cursor: 'pointer',
+                        color: '#fff',
+                    }}
+                >
+                    Connect to Hubspot
+                </button>
             </header>
         </div>
     );
