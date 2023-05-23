1ed35ab94b474fb309d00069598ff24b700b2320
Jatin Sandilya <7681067+jatinsandilya@users.noreply.github.com>
2023-05-23 13:52:22 +0530

    feat: add default rate-limits (#64)

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -8,11 +8,22 @@ import AuthService from './services/auth';
 import { register } from './generated/typescript';
 import { metadataService } from './services/metadata';
 
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 60, // Limit each IP to 60 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: async () => {
+        return JSON.stringify({ message: 'Rate limit reached.' });
+    },
+});
+
 const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
+app.use(limiter);
 app.use('/', indexRouter);
 app.use('/v1/crm', cors(), revertAuthMiddleware(), crmRouter);
 register(app, { metadata: metadataService });
