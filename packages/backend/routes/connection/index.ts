6d1302008e30f74cc9bf0ed5cd6818bdc5b79804
jatin <sandilya.jatin@gmail.com>
2023-06-14 14:27:48 +0530

    feat: add get all connections endpoint'

--- packages/backend/routes/connection/index.ts
+++ packages/backend/routes/connection/index.ts
@@ -23,5 +23,19 @@ connectionRouter.get('/', tenantMiddleware(), async (req, res) => {
     }
 });
 
+connectionRouter.get('/all', tenantMiddleware(), async (req, res) => {
+    try {
+        const result = await ConnectionService.getAllConnections(req, res);
+        if (result.error) {
+            res.status(400).send(result);
+        } else {
+            res.send(result);
+        }
+    } catch (error) {
+        console.error('Could not fetch leads', error);
+        res.status(500).send({ error: 'Internal server error' });
+    }
+});
+
 export default connectionRouter;
 export { connectionRouter };
