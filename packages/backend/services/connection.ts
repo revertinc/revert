c550e9e10e8d9a8a572fc520f2f1488cf9e9112c
Jatin Sandilya <7681067+jatinsandilya@users.noreply.github.com>
2023-06-14 14:29:36 +0530

    feat: add get all connections endpoint' (#76)

--- packages/backend/services/connection.ts
+++ packages/backend/services/connection.ts
@@ -1,5 +1,6 @@
 import { Request, ParamsDictionary, Response } from 'express-serve-static-core';
 import { ParsedQs } from 'qs';
+import prisma from '../prisma/client';
 
 class ConnectionService {
     async getConnection(
@@ -12,6 +13,30 @@ class ConnectionService {
         } else {
             return {
                 error: 'Connection not found!',
+            };
+        }
+    }
+    async getAllConnections(
+        _req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>,
+        res: Response<any, Record<string, any>, number>
+    ) {
+        const connection = res.locals.connection;
+        const tenantId = connection.t_id;
+        const connections: any = await prisma.connections.findMany({
+            where: { t_id: tenantId },
+            select: {
+                tp_id: true,
+                tp_access_token: true,
+                tp_refresh_token: true,
+                tp_customer_id: true,
+                t_id: true,
+            },
+        });
+        if (connections.length > 0) {
+            return connections;
+        } else {
+            return {
+                error: 'Connections not found!',
             };
         }
     }
