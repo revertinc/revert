e5b6f642f9e4c92067668c2f98424b4bd5218bcd
Jatin Sandilya <7681067+jatinsandilya@users.noreply.github.com>
2023-07-21 12:23:46 +0530

    dynamic environments on ui (#153)

--- packages/client/src/home/index.tsx
+++ packages/client/src/home/index.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './navbar';
 import ApiKeys from './apiKeys';
 import KeyIcon from '@mui/icons-material/Key';
 import AppsIcon from '@mui/icons-material/Apps';
 import Integrations from './integrations';
+import { useUser } from '@clerk/clerk-react';
+import { REVERT_BASE_API_URL } from '../constants';
 
 const Home = () => {
     const [tabValue, setTabValue] = React.useState(0);
+    const [account, setAccount] = React.useState<any>();
     const [environment, setEnvironment] = React.useState<string>('production');
+    const user = useUser();
+
+    useEffect(() => {
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        const data = JSON.stringify({
+            userId: user.user?.id,
+        });
+        const requestOptions = {
+            method: 'POST',
+            body: data,
+            headers: headers,
+        };
+        fetch(`${REVERT_BASE_API_URL}/internal/account`, requestOptions)
+            .then((response) => response.json())
+            .then((result) => {
+                setAccount(result?.account);
+            })
+            .catch((error) => {
+                console.log('error', error);
+            });
+    }, [user.user?.id]);
 
     const handleChange = (newTabValue: number) => {
         setTabValue(newTabValue);
@@ -23,7 +49,7 @@ const Home = () => {
 
     return (
         <>
-            <Navbar environment={environment} setEnvironment={setEnvironment} />
+            <Navbar environment={environment} setEnvironment={setEnvironment} environmentList={account?.environments} />
             <div className="flex h-[100%]">
                 <div className="w-[20%] flex flex-col items-center pt-[120px] text-[#6e6e6e]">
                     <ul>
