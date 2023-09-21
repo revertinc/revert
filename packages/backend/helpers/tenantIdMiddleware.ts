c8010d577873565d778d43bf958452690c8c33aa
jatin <sandilya.jatin@gmail.com>
2023-09-21 15:15:04 +0800

    chore: move logs to a single func

--- packages/backend/helpers/tenantIdMiddleware.ts
+++ packages/backend/helpers/tenantIdMiddleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { xprisma } from '../prisma/client';
-import logError from './logger';
+import logError, { logInfo } from './logger';
 
 const revertTenantMiddleware = () => async (req: Request, res: Response, next: () => any) => {
     const { 'x-revert-t-id': tenantId, 'x-revert-api-token': token } = req.headers;
@@ -37,7 +37,7 @@ const revertTenantMiddleware = () => async (req: Request, res: Response, next: (
             },
         });
         if (!connection || !connection.length) {
-            console.log(`Tenant not found ${tenantId}`);
+            logInfo(`Tenant not found ${tenantId}`);
             return res.status(400).send({
                 error: 'Tenant not found',
             });
