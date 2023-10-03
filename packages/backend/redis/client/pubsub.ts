f229f6383fee4c8233f05bee7732bd008def51ef
Jatin Sandilya <7681067+jatinsandilya@users.noreply.github.com>
2023-10-03 17:03:57 +0530

    fix: handle redis connections gracefully (#278)

--- packages/backend/redis/client/pubsub.ts
+++ packages/backend/redis/client/pubsub.ts
@@ -1,14 +1,14 @@
-import { createClient } from 'redis';
 import config from '../../config';
+import { createRedisClient } from '.';
 
 const subscribe = async (channelName: string, callback: any) => {
-    const subscriber = createClient({ url: config.REDIS_URL });
+    const subscriber = createRedisClient(config.REDIS_URL);
     await subscriber.connect();
     await subscriber.subscribe(channelName, callback);
 };
 
 const publish = async (channelName: string, data: any) => {
-    const publisher = createClient({ url: config.REDIS_URL });
+    const publisher = createRedisClient(config.REDIS_URL);
     await publisher.connect();
     await publisher.publish(channelName, JSON.stringify(data));
 };
@@ -23,7 +23,7 @@ export const PUBSUB_CHANNELS = {
 
 export interface IntegrationStatusSseMessage {
     publicToken: string;
-    status: "SUCCESS" | "FAILED";
+    status: 'SUCCESS' | 'FAILED';
     integrationName: string;
     tenantId: string;
     tenantSecretToken?: string;
