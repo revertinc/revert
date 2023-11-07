2be5b23653015f1a1e968523531da134a6a623a7
ashutoshdhande <50861363+ashutoshdhande@users.noreply.github.com>
2023-11-07 19:59:12 +0530

    chore: dynamic-os-script (#350)

--- packages/vue/src/App.vue
+++ packages/vue/src/App.vue
@@ -1,31 +1,31 @@
 <template>
-  <div class="container">
-    <RevertConnect :config="config" />
-  </div>
+    <div class="container">
+        <RevertConnect :config="config" />
+    </div>
 </template>
 
 <script>
 import RevertConnect from './components/RevertConnect.vue';
 export default {
-  name: 'App',
-  components: {
-    RevertConnect,
-  },
-  data() {
-    return {
-      config: {
-        revertToken: 'pk_test_Y2xlcmsuc3Ryb25nLmRlZXItNTYubGNsLmRldiQ',
-        tenantId: 'testTenantId',
-      },
-    };
-  },
+    name: 'App',
+    components: {
+        RevertConnect,
+    },
+    data() {
+        return {
+            config: {
+                revertToken: 'localPublicToken',
+                tenantId: 'testTenantId',
+            },
+        };
+    },
 };
 </script>
 
 <style scoped>
 .container {
-  margin: 2rem;
-  display: flex;
-  justify-content: center;
+    margin: 2rem;
+    display: flex;
+    justify-content: center;
 }
 </style>
