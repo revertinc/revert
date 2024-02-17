7c93ec5200e204b88bacf8f4aa729e3bba9cf86f
Samuel Olamide <sam99kupo@gmail.com>
2024-02-17 15:05:15 -0500

    fix: fix wrong imports

--- packages/backend/routes/v1/crm/utils/ms_dynamic_365.ts
+++ packages/backend/routes/v1/crm/utils/ms_dynamic_365.ts
@@ -6,7 +6,7 @@ import { Prisma, xprisma } from '../../../../prisma/client';
 import { TP_ID } from '@prisma/client';
 import pubsub, { IntegrationStatusSseMessage, PUBSUB_CHANNELS } from '../../../../redis/client/pubsub';
 import { AppConfig, IntegrationAuthProps, mapIntegrationIdToIntegrationName } from '../../../../constants/common';
-import sendIntegrationStatusError from 'routes/v1/sendIntegrationstatusError';
+import sendIntegrationStatusError from '../../sendIntegrationstatusError';
 
 const handleMsDynamicAuth = async ({
     account,
