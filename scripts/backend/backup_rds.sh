14445d9ef224c5126a6ff4ba4398a4fc31a13098
jatin <sandilya.jatin@gmail.com>
2024-03-18 17:21:51 +0530

    fix: db backup script paths

--- scripts/backend/backup_rds.sh
+++ scripts/backend/backup_rds.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export TABLE=accounts; bash ./scripts/backup_table.sh
-export TABLE=environments ; bash scripts/backup_table.sh
-export TABLE=apps ; bash scripts/backup_table.sh
-export TABLE=users ; bash scripts/backup_table.sh
-export TABLE=waitlist ; bash scripts/backup_table.sh
-export TABLE=connections ; bash scripts/backup_table.sh
+export TABLE=accounts; bash ./backup_table.sh
+export TABLE=environments ; bash ./backup_table.sh
+export TABLE=apps ; bash ./backup_table.sh
+export TABLE=users ; bash ./backup_table.sh
+export TABLE=waitlist ; bash ./backup_table.sh
+export TABLE=connections ; bash ./backup_table.sh
