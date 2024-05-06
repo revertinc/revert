1a919e323a241b474814a555f731efe69c024a38
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-05-06 11:50:38 +0530

    feat: onSelect Creation

--- packages/client/src/features/integration/AddIntegration.tsx
+++ packages/client/src/features/integration/AddIntegration.tsx
@@ -1,7 +1,8 @@
 import { Box } from '@mui/material';
 import Modal from '@mui/material/Modal';
-import React from 'react';
+import React, { useState } from 'react';
 import { appsInfo } from './enums/metadata';
+import { LoadingButton } from '@mui/lab';
 
 function AddIntegration({
     values,
@@ -13,58 +14,84 @@ function AddIntegration({
         apps: any;
     };
 }) {
+    const [selected, setSelected] = useState<string>('');
     const { init, setInit, handleCreation, apps } = values;
     const appsId = apps.map((app) => app.tp_id);
     return (
         <Modal
             open={init}
-            onClose={() => setInit(false)}
+            onClose={() => {
+                setInit(false);
+                setSelected('');
+            }}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
-            sx={{ backgroundColor: '#293347', width: '70vw', margin: '10vh 20vw 10vh 20vw' }}
-            className="rounded-xl overflow-scroll"
+            sx={{ backgroundColor: '#293347', width: '70vw', margin: '10vh 20vw 10vh 20vw', maxHeight: '74vh' }}
+            className="rounded-xl overflow-hidden"
         >
             <>
-                <Box component="div" sx={{ margin: '3rem' }}>
-                    <h1 className="text-3xl font-bold text-[#fff]">Create Integration</h1>
-                    <span className="text-[#b1b8ba]">Click to Configure New Integration</span>
-                </Box>
-                <div className="grid grid-cols-4 gap-8 justify-center content-center mx-8">
-                    {Object.keys(appsInfo).map((app, index) => {
-                        const isAppExist = appsId.includes(app);
-                        return (
-                            <Box
-                                key={index}
-                                sx={{
-                                    display: 'flex',
-                                    justifyContent: 'space-between',
-                                    cursor: 'pointer',
-                                    pointerEvents: isAppExist ? 'none' : 'initial',
-                                    backgroundColor: isAppExist && '#181d28',
-                                }}
-                            >
-                                <div
-                                    className="flex w-full justify-around items-center px-8 py-8 rounded-lg"
-                                    style={{
-                                        border: isAppExist ? '1px #ff8787 solid' : '0.5px #ced4da solid',
-                                    }}
-                                    onClick={() => {
-                                        handleCreation(app);
-                                    }}
-                                >
-                                    <img
-                                        width={100}
-                                        style={{
-                                            height: 40,
-                                            objectFit: 'scale-down',
+                <div className="overflow-hidden">
+                    <Box component="div" sx={{ width: '70vw', padding: '2rem 3rem 0rem 3rem', marginBottom: '1.6rem' }}>
+                        <h1 className="text-3xl font-bold text-[#fff]">Create Integration</h1>
+                        <span className="text-[#b1b8ba]">Click to Configure New Integration</span>
+                    </Box>
+                    <div className="pt-4 h-[36rem] overflow-scroll">
+                        <div className="grid grid-cols-4 gap-8 justify-center content-center mx-8 ">
+                            {Object.keys(appsInfo).map((app, index) => {
+                                const isAppExist = appsId.includes(app);
+                                return (
+                                    <Box
+                                        key={index}
+                                        sx={{
+                                            display: 'flex',
+                                            justifyContent: 'space-between',
+                                            cursor: 'pointer',
+                                            pointerEvents: isAppExist ? 'none' : 'initial',
+                                            backgroundColor: isAppExist && '#181d28',
                                         }}
-                                        alt={`${appsInfo[app].name} logo`}
-                                        src={appsInfo[app].logo}
-                                    />
-                                </div>
-                            </Box>
-                        );
-                    })}
+                                    >
+                                        <div
+                                            className="flex w-full justify-around items-center px-8 py-8 rounded-lg"
+                                            style={{
+                                                boxShadow:
+                                                    app === selected
+                                                        ? '0 0 0 3px #fff'
+                                                        : isAppExist
+                                                        ? '0 0 0 1px #ffa8a8'
+                                                        : '0 0 0 1px #ced4da',
+                                            }}
+                                            onClick={() => setSelected(app)}
+                                        >
+                                            <img
+                                                width={100}
+                                                style={{
+                                                    height: 40,
+                                                    objectFit: 'scale-down',
+                                                }}
+                                                alt={`${appsInfo[app].name} logo`}
+                                                src={appsInfo[app].logo}
+                                            />
+                                        </div>
+                                    </Box>
+                                );
+                            })}
+                        </div>
+                    </div>
+                    <div className="flex justify-end mr-8">
+                        <LoadingButton
+                            variant="contained"
+                            style={{
+                                background: '#293347',
+                                padding: '0.6rem 1.2rem',
+                            }}
+                            disabled={selected === ''}
+                            onClick={() => {
+                                handleCreation(selected);
+                            }}
+                        >
+                            Create
+                        </LoadingButton>
+                    </div>
                 </div>
             </>
         </Modal>
