cfa68beab2188de66483ed77b3d67e911e2195c8
shadab <shadab14meb346@gmail.com>
2024-05-09 03:15:56 +0530

    enhancement: store rate limiter instances in map with the key as subscription rate limit
    using this approach the stale rate limit issue is avoided

--- packages/backend/helpers/rateLimitMiddleware.ts
+++ packages/backend/helpers/rateLimitMiddleware.ts
@@ -4,14 +4,13 @@ import { RateLimiterRedis, IRateLimiterStoreOptions } from 'rate-limiter-flexibl
 import redis from '../redis/client';
 import { skipRateLimitRoutes } from './utils';
 
-const rateLimiters = new Map<string, RateLimiterRedis>();
+const rateLimiters = new Map<number, RateLimiterRedis>();
 
 //We can make this dynamic based on the subscription as well
 const RATE_LIMIT_DURATION_IN_MINUTES = 1;
 
-const getRateLimiter = (subscriptionId: string, rateLimit: number): RateLimiterRedis => {
-    //TODO: When the subscription data like rate limit changes in the DB we need to invalidate the rate limiter instance and create a new one
-    if (!rateLimiters.has(subscriptionId)) {
+const getRateLimiter = (rateLimit: number): RateLimiterRedis => {
+    if (!rateLimiters.has(rateLimit)) {
         const opts: IRateLimiterStoreOptions = {
             storeClient: redis,
             /** Dynamic points based on subscription.
@@ -20,9 +19,9 @@ const getRateLimiter = (subscriptionId: string, rateLimit: number): RateLimiterR
             points: rateLimit,
             duration: RATE_LIMIT_DURATION_IN_MINUTES * 60, // duration in seconds
         };
-        rateLimiters.set(subscriptionId, new RateLimiterRedis(opts));
+        rateLimiters.set(rateLimit, new RateLimiterRedis(opts));
     }
-    return rateLimiters.get(subscriptionId)!; // the `!` tells TypeScript that the return value is non-null
+    return rateLimiters.get(rateLimit)!; // the `!` tells TypeScript that the return value is non-null
 };
 
 async function rateLimitMiddleware(req: Request, res: Response, next: Function) {
@@ -30,11 +29,10 @@ async function rateLimitMiddleware(req: Request, res: Response, next: Function) 
     try {
         const { 'x-revert-t-id': tenantId } = req.headers;
         const { subscription } = res.locals.account;
-        const subscriptionId = subscription.id;
         const rateLimit = subscription.rate_limit;
         //TODO: Maybe include the x-revert-api-key along with the tenantId to make it more unique
-        const rateLimiter = getRateLimiter(subscriptionId, rateLimit);
-        await rateLimiter.consume(tenantId as string, 1);
+        const rateLimiter = getRateLimiter(rateLimit);
+        await rateLimiter.consume(tenantId as string, 1); // consume 1 point for each request
         next();
     } catch (rejRes) {
         if (rejRes instanceof Error) {
