5fb238ade3085810a42410ec308252bc2731886e
shadab <shadab14meb346@gmail.com>
2024-05-13 21:00:01 +0530

    refactor: use singleton approach for caching rateLimiter instances

--- packages/backend/helpers/rateLimitMiddleware.ts
+++ packages/backend/helpers/rateLimitMiddleware.ts
@@ -5,28 +5,25 @@ import redis from '../redis/client';
 import { skipRateLimitRoutes } from './utils';
 import config from '../config';
 
+// In Memory Cache for storing RateLimiterRedis instances to prevent the creation of a new instance for each request.
+// The cache key is the 'rate_limit' value derived from the subscriptions table. Currently, we cache by the 'rate_limit' value for simplicity.
+// Using 'subscriptionId' as a key would be more precise but would add complexity in keeping the cache in sync with the database.
+const rateLimiters = new Map<number, RateLimiterRedis>();
+
 //We can make this dynamic based on the subscription as well
 const RATE_LIMIT_DURATION_IN_MINUTES = 1;
 
-class RateLimiterManager {
-    // In Memory Cache for storing RateLimiterRedis instances to prevent the creation of a new instance for each request.
-    // The cache key is the 'rate_limit' value derived from the subscriptions table. Currently, we cache by the 'rate_limit' value for simplicity.
-    // Using 'subscriptionId' as a key would be more precise but would add complexity in keeping the cache in sync with the database.
-    #rateLimiters = new Map<number, RateLimiterRedis>();
-
-    get(rateLimit: number): RateLimiterRedis {
-        if (!this.#rateLimiters.has(rateLimit)) {
-            const opts: IRateLimiterStoreOptions = {
-                storeClient: redis,
-                points: rateLimit, // Points represent the maximum number of requests allowed within the set duration.
-                duration: RATE_LIMIT_DURATION_IN_MINUTES * 60, // Converts minutes to seconds for the duration.
-            };
-            this.#rateLimiters.set(rateLimit, new RateLimiterRedis(opts));
-        }
-        return this.#rateLimiters.get(rateLimit)!;
+const getRateLimiter = (rateLimit: number): RateLimiterRedis => {
+    if (!rateLimiters.has(rateLimit)) {
+        const opts: IRateLimiterStoreOptions = {
+            storeClient: redis,
+            points: rateLimit, // Points represent the maximum number of requests allowed within the set duration.
+            duration: RATE_LIMIT_DURATION_IN_MINUTES * 60, // Converts minutes to seconds for the duration.
+        };
+        rateLimiters.set(rateLimit, new RateLimiterRedis(opts));
     }
-}
-const rateLimiterManager = new RateLimiterManager();
+    return rateLimiters.get(rateLimit)!;
+};
 
 const rateLimitMiddleware = () => async (req: Request, res: Response, next: Function) => {
     if (skipRateLimitRoutes(req)) next();
@@ -34,7 +31,7 @@ const rateLimitMiddleware = () => async (req: Request, res: Response, next: Func
         const { 'x-revert-t-id': tenantId } = req.headers;
         const { subscription, id: accountId } = res.locals.account; // Subscription details are retrieved from response locals set earlier in the revertAuthMiddleware.
         const rateLimit = subscription?.rate_limit ?? config.DEFAULT_RATE_LIMIT_DEVELOPER_PLAN; //incase subscription undefined, we will use the default rate limit this is to make sure backward compatibility as currently some accounts might not have subscription attached to them. We can remove the optional chaining and nullish coalescing once we are sure that all accounts have subscription attached to them
-        const rateLimiter = rateLimiterManager.get(rateLimit);
+        const rateLimiter = getRateLimiter(rateLimit);
         //added accountId to make the key unique
         const uniqueKey = `${accountId}-${tenantId}`;
         // rate limit is per tenantId
