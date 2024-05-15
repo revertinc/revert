e5a9340a694d98f8dd6fd7d46350ac387fdcd572
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-05-15 10:13:24 +0530

    fix: connecting to revert on null props

--- packages/react/src/lib/useRevertConnect.ts
+++ packages/react/src/lib/useRevertConnect.ts
@@ -13,6 +13,7 @@ if (typeof window !== 'undefined') {
 }
 
 declare var __CDN_PATH__: string;
+const REVERT_TOKEN_LENGTH = 44;
 
 export function useRevertConnectScript() {
     const [loading, setLoading] = useState(true);
@@ -45,9 +46,17 @@ export function useRevertConnectScript() {
 export default function useRevertConnect(props: useRevertConnectProps) {
     const { loading, error } = useRevertConnectScript();
     const [integrationsLoaded, setIntegrationsLoaded] = useState(false);
+    const { tenantId, revertToken } = props.config;
 
     useEffect(() => {
-        if (!loading && typeof window !== 'undefined' && window.Revert && window.Revert.init) {
+        /* Todo: 
+            This is temporary solution and requires more to be done, We need a mechanism to fetch as we render (before paint), 
+            to know if revertToken and tenantId passed exist and its true.        
+        */
+        const isValidRevertToken = revertToken.length === REVERT_TOKEN_LENGTH;
+        const isValidTenantId = tenantId != 'null' && tenantId != undefined;
+        const isConfigPropValid = isValidRevertToken && isValidTenantId;
+        if (!loading && typeof window !== 'undefined' && window.Revert && window.Revert.init && isConfigPropValid) {
             window.Revert.init({
                 ...props.config,
                 onLoad: () => {
