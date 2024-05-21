f7054f8071e1478197ae47b315fef507cf78f6f0
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-05-21 16:31:59 +0530

    fix: deleting svix account to handle only delete which is existing

--- packages/backend/services/svix.ts
+++ packages/backend/services/svix.ts
@@ -1,33 +1,45 @@
+import { logError } from 'helpers/logger';
 import config from '../config';
 import prisma from '../prisma/client';
 class SvixService {
     // clerk userId
     async deleteAssociatedSvixAccountForUser(userId: string): Promise<any> {
-        const accountInformation = await prisma.users.findUnique({
-            where: {
-                id: userId,
-            },
-            select: {
-                account: {
-                    select: {
-                        id: true,
-                        environments: {
-                            select: {
-                                env: true,
+        try {
+            const accountInformation = await prisma.users.findUnique({
+                where: {
+                    id: userId,
+                },
+                select: {
+                    account: {
+                        select: {
+                            id: true,
+                            environments: {
+                                select: {
+                                    env: true,
+                                },
                             },
                         },
                     },
                 },
-            },
-        });
+            });
 
-        const accountId = accountInformation?.account.id!;
+            const accountId = accountInformation?.account.id!;
 
-        // delete all the associated svixAccount
-        accountInformation?.account.environments.map(async (e) => {
-            const svixId = `${accountId}_${e.env}`;
-            await config.svix?.application.delete(svixId);
-        });
+            // delete all the associated svixAccount
+            accountInformation?.account.environments.map(async (e) => {
+                const svixId = `${accountId}_${e.env}`;
+                try {
+                    await config.svix?.application.delete(svixId);
+                } catch (error) {
+                    // svixAppId wasn't existing
+                }
+            });
+        } catch (error) {
+            logError({
+                message: `Error while deleting Associated Svix Account For ${userId}`,
+                name: 'SvixAccountDeletion',
+            });
+        }
     }
 }
 
