6d9bdcb4fdd57e11865d999e10bab416b0ca7836
Jatin Sandilya <7681067+jatinsandilya@users.noreply.github.com>
2024-05-23 12:24:30 +0530

    fix: rate limiter not skipping routes (#573)

--- packages/backend/helpers/rateLimitMiddleware.ts
+++ packages/backend/helpers/rateLimitMiddleware.ts
@@ -27,10 +27,12 @@ const getRateLimiter = (rateLimit: number): RateLimiterRedis => {
 };
 
 const rateLimitMiddleware = () => async (req: Request, res: Response, next: Function) => {
-    if (skipRateLimitRoutes(req)) next();
+    if (skipRateLimitRoutes(req)) {
+        return next();
+    }
     try {
         const { 'x-revert-t-id': tenantId } = req.headers;
-        const { subscription, id: accountId } = res.locals.account; // Subscription details are retrieved from response locals set earlier in the revertAuthMiddleware.
+        const { subscription, id: accountId } = res.locals.account || {}; // Subscription details are retrieved from response locals set earlier in the revertAuthMiddleware.
         const rateLimit =
             subscription?.rate_limit ?? config.DEFAULT_RATE_LIMIT_DEVELOPER_PLAN ?? FALL_BACK_DEFAULT_RATE_LIMIT; //incase subscription undefined, we will use the default rate limit this is to make sure backward compatibility as currently some accounts might not have subscription attached to them. We can remove the optional chaining and nullish coalescing once we are sure that all accounts have subscription attached to them. In case of DEFAULT_RATE_LIMIT_DEVELOPER_PLAN is missing in config, we will fallback to 1
         const rateLimiter = getRateLimiter(rateLimit);
