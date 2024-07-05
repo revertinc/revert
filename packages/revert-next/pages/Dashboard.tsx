f0448f4986b7b51b3e5ba794e48d5e8ee5f08dc1
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-07-05 16:06:26 +0530

    fix(ui): recent api calls card

--- packages/revert-next/pages/Dashboard.tsx
+++ packages/revert-next/pages/Dashboard.tsx
@@ -34,37 +34,32 @@ function Dashboard({ userId }: { userId: string }) {
                         </p>
                     </div>
                     <div className="grid grid-cols-2 text-xs">
-                        <div>
-                            <h3 className="uppercase text-gray-50/80 font-bold mb-2">endpoint</h3>
-                            <div className="flex flex-col gap-3 items-start justify-center">
+                        <div className="col-span-2">
+                            <div className="flex justify-between">
+                                <h3 className="uppercase text-gray-50/80 font-bold mb-3">endpoint</h3>
+                                <h3 className="uppercase text-gray-50/80 font-bold mb-3">enabled</h3>
+                            </div>
+                            <div className="flex flex-col gap-3">
                                 {recentCalls?.length ? (
                                     recentCalls.map((c) => {
                                         return (
-                                            <div className="flex items-center" key={uuid()}>
-                                                <Badge variant={c.method}> GET </Badge>
-                                                <p className="ml-2 text-gray-50/70">{c.path}</p>
+                                            <div key={uuid()} className="flex justify-between items-center">
+                                                <div className="flex items-center">
+                                                    <Badge variant={c.method}> GET </Badge>
+                                                    <p className="ml-2 text-gray-50/70">{c.path}</p>
+                                                </div>
+                                                <div
+                                                    className={cn('bg-green-500 w-3 h-3 rounded-full mr-5', {
+                                                        'bg-red-500': !new String(c.status).startsWith('2'),
+                                                    })}
+                                                    key={uuid()}
+                                                ></div>
                                             </div>
                                         );
                                     })
                                 ) : (
                                     <p className="text-gray-50/70 font-semibold">No API Calls</p>
                                 )}
-                            </div>
-                        </div>
-                        <div className="justify-self-end">
-                            <h3 className="uppercase text-gray-50/80 font-bold mb-3">enabled</h3>
-                            <div className="flex items-center flex-col gap-6 justify-center">
-                                {recentCalls?.length > 0 &&
-                                    recentCalls.map((c) => {
-                                        return (
-                                            <div
-                                                className={cn('bg-green-500 w-3 h-3 rounded-full mt-0.5', {
-                                                    'bg-red-500': !new String(c.status).startsWith('2'),
-                                                })}
-                                                key={uuid()}
-                                            />
-                                        );
-                                    })}
                             </div>
                         </div>
                     </div>
