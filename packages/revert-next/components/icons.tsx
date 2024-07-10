899e3fa80331f2d52d8f76718e4a33d318808fd1
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-07-10 15:52:40 +0530

    feat: delete integration container

--- packages/revert-next/components/icons.tsx
+++ packages/revert-next/components/icons.tsx
@@ -219,6 +219,25 @@ export const Icons = {
             <path d="m21 21-4.3-4.3" />
         </svg>
     ),
+    trash: (props: IconProps) => (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-trash"
+            {...props}
+        >
+            <path d="M3 6h18" />
+            <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+            <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+        </svg>
+    ),
 };
 
 export {
