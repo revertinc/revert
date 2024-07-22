8e057063fa14e23991832d93f00225098c133c16
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-07-22 17:00:45 +0530

    fix: environment change under settings should redirect to /dashboard/integrations

--- packages/revert-next/components/ui/sidenav/EnvironmentMode.tsx
+++ packages/revert-next/components/ui/sidenav/EnvironmentMode.tsx
@@ -5,6 +5,7 @@ import { Icons } from '@revertdotdev/icons';
 import { Switch } from '@revertdotdev/components';
 import { useAnalytics } from '@revertdotdev/hooks';
 import { useEffect } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
 
 export function EnvironmentMode({
     isDefaultEnvironment,
@@ -16,6 +17,9 @@ export function EnvironmentMode({
     prodPrivateToken: string;
 }) {
     const { mutate } = useAnalytics(userId);
+    const pathname = usePathname();
+    const router = useRouter();
+    const isSettings = pathname?.includes('config');
 
     useEffect(
         function () {
@@ -34,6 +38,9 @@ export function EnvironmentMode({
                 checked={isDefaultEnvironment}
                 onClick={async () => {
                     await changeEnvironmentMode();
+                    if (isSettings) {
+                        router.push('/dashboard/integrations');
+                    }
                     setTimeout(async () => {
                         await mutate();
                     }, 1000);
