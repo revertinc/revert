969dee9ebb432924060623c798d044305087e31d
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-07-22 16:07:15 +0530

    fix(ui): add integrations logos

--- packages/revert-next/lib/constants.ts
+++ packages/revert-next/lib/constants.ts
@@ -1,58 +1,59 @@
+import { Socials } from '@revertdotdev/icons';
 export const REVERT_BASE_API_URL = 'http://localhost:4001';
 
 export const DEFAULT_ENV = 'development';
-export const appsInfo: Record<string, { name: string; logo: string }> = {
+export const appsInfo: Record<string, { name: string; logo: JSX.Element }> = {
     hubspot: {
         name: 'Hubspot',
-        logo: 'https://res.cloudinary.com/dfcnic8wq/image/upload/v1711548892/Revert/v8xy74cep10cjuitlnpk.png',
+        logo: Socials.hubspot(),
     },
     sfdc: {
         name: 'Salesforce',
-        logo: 'https://res.cloudinary.com/dfcnic8wq/image/upload/v1711548963/Revert/by6ckdbnibuniorebwxj.png',
+        logo: Socials.sfdc(),
     },
     zohocrm: {
         name: 'ZohoCRM',
-        logo: 'https://res.cloudinary.com/dfcnic8wq/image/upload/v1711549106/Revert/lig4v85hfhshob9w6z9z.png',
+        logo: Socials.zohocrm(),
     },
     pipedrive: {
         name: 'Pipedrive',
-        logo: 'https://res.cloudinary.com/dfcnic8wq/image/upload/v1711548714/Revert/opggbicfjuskkxnflysm.png',
+        logo: Socials.pipedrive(),
     },
     closecrm: {
         name: 'Close CRM',
-        logo: 'https://res.cloudinary.com/dfcnic8wq/image/upload/v1711548783/Revert/mrfg9qcxzh5r2iyatjdg.png',
+        logo: Socials.closecrm(),
     },
     ms_dynamics_365_sales: {
         name: 'MS Dynamics Sales',
-        logo: 'https://res.cloudinary.com/dfcnic8wq/image/upload/v1711549741/Revert/pbvr2f2yszrt5ithbirb.png',
+        logo: Socials.ms_dynamics_365_sales(),
     },
     slack: {
         name: 'Slack',
-        logo: 'https://res.cloudinary.com/dfcnic8wq/image/upload/v1711550376/Revert/gei0ux6iptaf1nfxjfv2.png',
+        logo: Socials.slack(),
     },
     discord: {
         name: 'Discord',
-        logo: 'https://res.cloudinary.com/dfcnic8wq/image/upload/v1711550278/Revert/sgbdv2n10bajbykvtxl4.png',
+        logo: Socials.discord(),
     },
     linear: {
         name: 'Linear',
-        logo: 'https://res.cloudinary.com/dfcnic8wq/image/upload/v1711549244/Revert/v8r7gnqe0tzoozwbhnyn.png',
+        logo: Socials.linear(),
     },
     clickup: {
         name: 'Clickup',
-        logo: 'https://res.cloudinary.com/dfcnic8wq/image/upload/v1711549293/Revert/ooo7iegqcrdkxgrclzjt.png',
+        logo: Socials.clickup(),
     },
     jira: {
         name: 'Jira',
-        logo: 'https://res.cloudinary.com/dfcnic8wq/image/upload/v1711549557/Revert/tsjway6elov5bv1tc5tk.png',
+        logo: Socials.jira(),
     },
     trello: {
         name: 'Trello',
-        logo: 'https://res.cloudinary.com/dfcnic8wq/image/upload/v1711549291/Revert/caydzlxzcitdu2n9yuea.png',
+        logo: Socials.trello(),
     },
 
     bitbucket: {
         name: 'Bitbucket',
-        logo: 'https://res.cloudinary.com/dfcnic8wq/image/upload/v1711549311/Revert/cmqpors8m8tid9zpn9ak.png',
+        logo: Socials.bitbucket(),
     },
 };
