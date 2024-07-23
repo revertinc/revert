3673b9c57f153a67dd443050c022e491966fa9a2
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-07-23 18:23:13 +0530

    fix(ui): change commandItem color on hover

--- packages/revert-next/components/ui/common/Command.tsx
+++ packages/revert-next/components/ui/common/Command.tsx
@@ -107,7 +107,7 @@ const CommandItem = React.forwardRef<
     <CommandPrimitive.Item
         ref={ref}
         className={cn(
-            'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:opacity-50',
+            'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:opacity-50 hover:bg-gray-25',
             className
         )}
         {...props}
