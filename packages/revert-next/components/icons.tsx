91125869f748394f8d15cc499f0c739093d58037
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-07-26 11:44:44 +0530

    chore: include loader svg

--- packages/revert-next/components/icons.tsx
+++ packages/revert-next/components/icons.tsx
@@ -274,6 +274,22 @@ export const Icons = {
             <path d="m6 9 6 6 6-6" />
         </svg>
     ),
+    loader: (props: IconProps) => (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            {...props}
+        >
+            <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+        </svg>
+    ),
 };
 
 export {
