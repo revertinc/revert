a0c0855c080e0acd47dcbf27610fd897a30f617c
manish-singh-bisht <mthefool218@gmail.com>
2024-07-31 15:20:21 +0530

    revert dashboard github image link

--- packages/client/src/features/integration/enums/metadata.ts
+++ packages/client/src/features/integration/enums/metadata.ts
@@ -70,7 +70,7 @@ export const appsInfo = {
 
     github: {
         name: 'GitHub',
-        logo: 'https://res.cloudinary.com/dfcnic8wq/image/upload/v1711549311/Revert/cmqpors8m8tid9zpn9ak.png',
+        logo: 'https://res.cloudinary.com/dwoiwg0t5/image/upload/v1722418192/RevertAppLogos/cmeafzqvnvvconohfnnr.png',
         description: 'Configure your GitHub Ticketing App from here.',
     },
     // asana: {
